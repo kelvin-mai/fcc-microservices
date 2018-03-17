be740ed8d055eec3d681c547b5503c08c36a4de0
Kelvin Moi <kelvin.mai002@gmail.com>
2018-03-17 16:34:20 -0500

    Finished API

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,38 @@ const router = require('express').Router(),
 
 const db = require('../models');
 
-router.route('/').get((req, res) => {
-	const { url } = req.query;
-	res.json({ url });
-});
+router
+	.route('/*')
+	.get((req, res) => {
+		const url = req.url.replace('/', '');
+		if (valid.isWebUri(url)) {
+			db.Url.findOne({ original: url })
+				.then(link => {
+					if (!link) res.json({ message: 'not found' });
+					res.json(link);
+				})
+				.catch(err => res.json(err));
+		} else {
+			db.Url.findOne({ short: url })
+				.then(link => {
+					if (!link) res.json({ message: 'not found' });
+					res.json(link);
+				})
+				.catch(err => res.json(err));
+		}
+	})
+	.post((req, res) => {
+		const url = req.url.replace('/', '');
+		if (valid.isWebUri(url)) {
+			db.Url.create({
+				original: url,
+				short: short.shortener(url)
+			})
+				.then(link => res.json(link))
+				.catch(err => res.json(err));
+		} else {
+			res.json({ message: 'invalid url' });
+		}
+	});
 
 module.exports = router;
